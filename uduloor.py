c1fe5fd40f9b8605925113cc39bd7cf2dc2d782c
Märt Põder <boamaod@gmail.com>
2020-06-15 15:16:16 +0300

    Add dry run and use English by default.

--- uduloor.py
+++ uduloor.py
@@ -7,26 +7,33 @@ import time, random
 
 from collections import namedtuple
 
-url = "smtp.gmail.com"
-port = 465
-username = "boamaod"
-password = "secret123"
-
 sender = "pseudo@infoaed.ee"
+bulletin = "https://etherpad.wikimedia.org/p/psephos-demosia"
 
-message_template = """From: Uduloor <pseudo@infoaed.ee>
+message_template = """From: Uduloor <%s>
 To: %s
-Subject: Uduloor
+Subject: Your pseudonym for voting at the upcoming election
 
-Sinu pseudonüüm hääletamisel on:
+Your pseudonym for voting at the upcoming election is:
 
 * %s
 
-Hääletus toimub aadressil
+Voting takes place at:
 
-* https://etherpad.wikimedia.org/p/wmee-juhatus
+* %s
 
-Siiralt "jne"
+Guidelines in nutshell:
+
+* To cast your vote, please write down your pseudonym followed by your choice at the election.
+* Make sure you write this information on a separate line at the bulletin board to avoid any confusion.
+* To preserve your anonymity in this secret election you *should not* log in to the bulletin board.
+* For extra caution you can use the private mode of your web browser or even special anonymity preserving browser.
+* Before voting is closed, please make sure your vote is still correctly displayed at the bulletin board.
+* After voting is closed you (or actually anyone) can tally the votes to make sure everything was done correctly.
+
+Happy voting!
+
+Sincereley "etc"
 Uduloor
 """
 
@@ -60,15 +67,24 @@ curcuma
 antirrhinum
 """
 
+code_range = range(100, 999)
+
+DRY_RUN = True
+
+url = "smtp.gmail.com"
+port = 465
+username = "boamaod"
+password = "secret123"
+
 pseudo_id = namedtuple('pseudo_id', ['pseudonym', 'code', 'cryptonym'])
 
 voter_list = voters_in_text.strip().splitlines()
 word_list = unique_words.strip().splitlines()
 
 random_words = random.sample(word_list, len(voter_list))
-random_keys = random.sample(range(100, 999), len(voter_list))
+random_keys = random.sample(code_range, len(voter_list))
 
-print("There is %d voters in upcoming election:\n" % len(voter_list))
+print("There are %d voters in upcoming election:\n" % len(voter_list))
 
 for voter in voter_list:
   print(voter)
@@ -88,22 +104,25 @@ for i in range(len(voter_list)):
 i = -1
 
 try:
-    
-  #server = smtplib.SMTP_SSL(url, port, context=ssl.create_default_context())
-  #server.login(username, password)
+  
+  if not DRY_RUN:  
+    server = smtplib.SMTP_SSL(url, port, context=ssl.create_default_context())
+    server.login(username, password)
   
   for i in range(len(voter_list)):
 
     receiver = voter_list[i].split()[0]
-    message = message_template % (receiver, pseudo[i].cryptonym)
+    message = message_template % (sender, receiver, pseudo[i].cryptonym, bulletin)
     
     print("*", pseudo[i].pseudonym)
     
-    #server.sendmail(sender, receiver, message.encode("utf8"))
+    if not DRY_RUN:
+      server.sendmail(sender, receiver, message.encode("utf8"))
     
     time.sleep(1)
     
-  #server.quit()
+  if not DRY_RUN:
+    server.quit()
   
   print()
   
@@ -131,3 +150,7 @@ print()
 
 for i in range(len(voter_list)):
     print("*", pseudo[i].cryptonym)
+
+print()
+
+print("Thanks for taking digital democracy seriously!")
